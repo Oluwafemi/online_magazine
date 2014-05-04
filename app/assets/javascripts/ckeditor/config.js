73d8290297bb413c5d9b70e3d179bb6e525a1889
Oluwafemi <contextfree9@gmail.com>
2014-05-04 07:07:51 +0100

    resolved activeadmin-ckeditor style issue

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,8 +1,16 @@
 if (typeof(CKEDITOR) != 'undefined')
 {
   CKEDITOR.editorConfig = function(config) {
-    config.uiColor = "#AADC6E";
-    //config.toolbar = [ [ 'Source', 'Bold' ], ['CreatePlaceholder'] ];
+    //config.uiColor = "#AADC6E";
+    config.toolbar = [
+    [ 'Source', '-', 'NewPage', 'Preview', '-', 'Templates' ],
+    [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],
+    [ 'Bold', 'Italic' ]
+    ];
+    config.disableNativeSpellChecker = false;
+    config.toolbarCanCollapse = true;
+    config.pasteFromWordRemoveFontStyles = false;
+    config.scayt_autoStartup = true;
   }
 } else{
   console.log("ckeditor not loaded")
